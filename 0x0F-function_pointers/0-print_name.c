fd20f81cf5aa4450f271f02c9354646f67909583
ericono1 <ericono1@gmail.com>
2022-11-16 08:26:29 -0800

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,17 +3,11 @@
 
 
 /**
-
  * print_name - prints a name.
-
  * @name: input name.
-
  * @f: function pointer.
-
  *
-
  * Return: no return.
-
  */
 
 void print_name(char *name, void (*f)(char *))
